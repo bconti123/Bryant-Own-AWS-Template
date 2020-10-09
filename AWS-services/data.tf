a94e57305df23b0e6cd0dbd8d620acdc687b7a02
bconti123 <bconti00@gmail.com>
2020-10-08 20:23:19 -0700

    New branch

---
+++ AWS-services/data.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "latest-ubuntu" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
+data "aws_ami" "latest_ecs" {
+most_recent = true
+owners = ["591542846629"] # AWS
+
+  filter {
+      name   = "name"
+      values = ["*amazon-ecs-optimized"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }  
+}
