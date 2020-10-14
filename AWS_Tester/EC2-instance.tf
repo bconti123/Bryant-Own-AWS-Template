2f73d9701484c4c2b0e54e92f34609b26a9229c7
bconti123 <bconti00@gmail.com>
2020-10-14 08:04:20 -0700

    Saved VPC,tf

--- AWS_Tester/EC2-instance.tf
+++ AWS_Tester/EC2-instance.tf
@@ -7,8 +7,7 @@ resource "aws_instance" "my_latestecs_instance" {
     instance_type = "t2.micro" ## Pick which instance type you want
     # key_name = ## If you have key pair, put it here.
     subnet_id = "aws_vpc.main.id" ## Check vpc.tf
-    
-    # vpc_security_group_ids = 
+    vpc_security_group_ids = ""
 
     user_data = <<-EOF
         #! /bin/bash
