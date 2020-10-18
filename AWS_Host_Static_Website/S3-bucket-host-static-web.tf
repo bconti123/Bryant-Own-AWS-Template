d5f6c9a563ee6b05faa17ec935d01bb46ce6324f
bconti123 <47548036+bconti123@users.noreply.github.com>
2020-10-18 09:13:25 -0700

    Host-Static-Website

---
+++ AWS_Host_Static_Website/S3-bucket-host-static-web.tf
@@ -0,0 +1,45 @@
+
+## Create AWS S3 Bucket
+resource "aws_s3_bucket" "static-web" {
+  bucket = "bryantconti-tester.com"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+      [{
+          "Condition": {
+              "KeyPrefixEquals": "docs/"
+          },
+          "Redirect": {
+              "ReplaceKeyPrefixWith": "documents/"
+          }
+      }]
+    EOF
+  }
+}
+
+## CORS RULE
+/*  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+} */
+
+## VERSIONING CONTROL
+/* versioning {
+    enabled = true
+  }
+} */
+
+## LOGGING
+/*  logging {
+    target_bucket = aws_s3_bucket.log_bucket.id
+    target_prefix = "log/"
+  } */
