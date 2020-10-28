1a39c9174e96e336bc4343d648cab8e17964f413
bryant <bconti00@gmail.com>
2020-10-28 14:06:42 -0700

    Work Progress with S3 policy

--- AWS_Host_Static_Website/S3-bucket-host-static-web.tf
+++ AWS_Host_Static_Website/S3-bucket-host-static-web.tf
@@ -43,3 +43,28 @@ resource "aws_s3_bucket" "static-web" {
     target_bucket = aws_s3_bucket.log_bucket.id
     target_prefix = "log/"
   } */
+
+## Policy (NEED FIX THIS!)
+
+resource "aws_s3_bucket_policy" "b" { ## See first line, copy and paste this.
+  bucket = aws_s3_bucket.b.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Deny",
+      "Principal": "*",
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::my_tf_test_bucket/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
+      }
+    }
+  ]
+}
+POLICY
+}
