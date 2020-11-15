fb36f642c8fcea6b55eb1130d7736555c7c2d5e1
bconti123 <bconti00@gmail.com>
2020-11-15 09:50:22 -0800

    Test

--- AWS_Cloud9/AWS_Cloud9.tf
+++ AWS_Cloud9/AWS_Cloud9.tf
@@ -1,6 +1,6 @@
 
 ## AWS CLOUD9
-resource "aws_cloud9_environment_ec2" "example" {
+resource "aws_cloud9_environment_ec2" "example-name" {
   instance_type = "t2.micro"
   name          = "example-env"
 }
